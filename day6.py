487346683175d4b8122ba90585e3b82512ddf1b3
ridoy-diu <ridoy2305101148@diu.edu.bd>
2025-01-21 23:20:53 +0600

    day 6

---
+++ day6.py
@@ -0,0 +1,61 @@
+# indentation
+
+for i in range(5):
+    print(i)
+
+
+# Reeborg's world hurdle 
+
+# 3
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+
+
+# 4
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     turn_left()
+    
+#     while wall_on_right():
+#         move()
+        
+#     turn_right()
+#     move()
+#     turn_right()
+    
+#     while front_is_clear():
+#         move()
+        
+#     turn_left()
+    
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+    
