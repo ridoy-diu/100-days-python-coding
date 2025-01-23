028c28ff3dfe04502c9ff8487ea525a912b86d4b
ridoy-diu <ridoy2305101148@diu.edu.bd>
2025-01-24 01:34:47 +0600

    hangman game complete

--- project6.py
+++ project6.py
@@ -1,9 +1,60 @@
 # hangman game
 import random
 
-words = ["hello", "cat", "mouse", "dog", "laptop", "football"]
+hangman_art = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+words = ["hello", "cat", "mouse", "dog", "laptop", "football", "cricket", "doctor", "phone"]
 random_word = random.choice(words)
-print(random_word)
+# print(random_word)
 
 blank = ""
 for i in range(len(random_word)):
@@ -14,11 +65,23 @@ game_over = False
 
 previous_value_store = []
 
+lives = 6
+
 while not game_over:
 
+    print("You have ", lives, " lives left.")
     display = ""
     guess = input("Guess a letter: ").lower()
+    
+    if guess not in random_word:
+        print(f"You guessed {guess}, which is not in the word. So you lost a life.")
+        lives -= 1
+
+    if guess in previous_value_store:
+        print(f"You have already guessed {guess}")
+
     for letter in random_word:
+
         if letter == guess:
             display += letter
             previous_value_store += letter
@@ -28,8 +91,14 @@ while not game_over:
             display += "_"
 
     if "_" not in display:
-        print("You win")
+        print("********You Win!********")
         game_over = True
 
-    print(display)
+    if lives == 0:
+        print("********You Lose!********")
+        print("The correct word was ", random_word)
+        game_over = True
+
+    print("\nDISPLAY: ", display)
+    print(hangman_art[6 - lives])
 
