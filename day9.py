4494a5652d1bc9ea93ea5908194fb0159c36e884
ridoy-diu <ridoy2305101148@diu.edu.bd>
2025-01-25 15:05:16 +0600

    grading problem with dictionary

---
+++ day9.py
@@ -0,0 +1,39 @@
+# # dictionary
+
+# name_age_dictionary = {"ridoy": 23, "joy": 25}
+
+# print(name_age_dictionary["ridoy"])
+
+# name_age_dictionary["rafi"] = 24
+# print(name_age_dictionary)
+
+# for key in name_age_dictionary:
+#     print(key, name_age_dictionary[key])
+
+
+# Grading Problem
+
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades = {}
+
+for key in student_scores:
+    score = student_scores[key]
+    if score >= 91:
+        student_grades[key] = "Outstanding"
+    elif score >= 81:
+        student_grades[key] = "Exceeds Expectations"
+    elif score >= 71:
+        student_grades[key] = "Acceptable"
+    else:
+        student_grades[key] = "Fail"
+
+
+print(student_scores)
+print(student_grades)
